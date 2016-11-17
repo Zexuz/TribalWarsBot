4dfaf31f9eaa68f586a23af1db290d28d4c9e039
Zexuz <Robin.edbom@gmail.com>
2016-11-17 16:36:57 +0100

    Refactored

--- Program.cs
+++ Program.cs
@@ -63,35 +63,6 @@ namespace TribalWarsBot {
 //            Console.WriteLine(canselReqStatus ? "Canceling the order" : "Error, the order was not registered");
         }
 
-        public string SendAttackInit(RequestManager requestManager, int villageNr) {
-            var url = $"https://sv36.tribalwars.se/game.php?village={villageNr}&screen=place&try=confirm";
-            var postData =
-                "22c2d931d74cd8a06d92b4=e6c57ef722c2d9&template_id=&source_village=2173&spear=&sword=63&axe=&spy=&light=&heavy=&ram=&catapult=&knight=&snob=&x=525&y=473&target_type=coord&input=&attack=Attack";
-            var req = requestManager.GeneratePOSTRequest(url, postData, null, null, true);
-            var res = requestManager.GetResponse(req);
-
-            //post data 22c2d931d74cd8a06d92b4=e6c57ef722c2d9&template_id=&source_village=2173&spear=&sword=63&axe=&spy=&light=&heavy=&ram=&catapult=&knight=&snob=&x=525&y=473&target_type=coord&input=&attack=Attack
-            // Response html is in attacjResponseHtml
-            var str = RequestManager.GetResponseStringFromResponse(res);
-            CQ html = str;
-            var formElement = html.Select("#command-data-form");
-            var inputs = formElement.Children("input").Where(ele => ele.Type == "hidden").ToList();
-
-            var queryString = inputs.Aggregate("", (current, input) => current + $"{input.Name}={input.Value}&");
-            return queryString;
-        }
-
-
-        public void SendAttackConfirm(RequestManager requestManager, string postData,string token) {
-            var url = $"https://sv36.tribalwars.se/game.php?village=2173&screen=place&action=command&h={token}";
-            var req =requestManager.GeneratePOSTRequest(url, postData, null, null, true);
-            var res = requestManager.GetResponse(req);
-            var htmlResponse = RequestManager.GetResponseStringFromResponse(res);
-            //https://sv36.tribalwars.se/game.php?village=2173&screen=place&action=command&h=3d778f4e
-            //post data attack=true&ch=5691301055711e723e118f2f603014f44c1ba158&x=525&y=473&source_village=2173&action_id=110047&spear=0&sword=63&axe=0&spy=0&light=0&heavy=0&ram=0&catapult=0&knight=0&snob=0&building=main
-        }
-
-        public void PrepareToSendAttack() {}
 
 
         public static void Main(string[] args) {
--- Services/AttackService.cs
+++ Services/AttackService.cs
@@ -1,8 +1,59 @@
-﻿namespace TribalWarsBot.Services {
+﻿using System.Linq;
+
+using CsQuery;
+
+using TribalWarsBot.Helpers;
+
+namespace TribalWarsBot.Services {
 
     public class AttackService {
 
-        public bool SendAttack(RequestManager requestManager, PlanedAttack planedAttack) {}
+        private readonly RequestManager _requestManager;
+
+        public AttackService(RequestManager requestManager) {
+            _requestManager = requestManager;
+        }
+
+        public bool SendAttack(RootObject rootObject, PlanedAttack planedAttack) {
+            var postPayload = SendAttackInit(planedAttack, rootObject.village.id);
+            SendAttackConfirm(postPayload, rootObject.csrf);
+
+            return true;
+        }
+
+
+        private string SendAttackInit(PlanedAttack planedAttack, int villageNr) {
+            var url = $"https://sv36.tribalwars.se/game.php?village={villageNr}&screen=place&try=confirm";
+            var postData =
+                "22c2d931d74cd8a06d92b4=e6c57ef722c2d9" +
+                "&template_id=" +
+                $"&source_village={villageNr}" +
+                $"&{planedAttack.units}" +
+                $"&x={planedAttack.X}" +
+                $"&y={planedAttack.Y}" +
+                "&target_type=coord" +
+                "&input=&attack=Attack";
+            var req = _requestManager.GeneratePOSTRequest(url, postData, null, null, true);
+            var res = _requestManager.GetResponse(req);
+
+            //post data 22c2d931d74cd8a06d92b4=e6c57ef722c2d9&template_id=&source_village=2173&spear=&sword=63&axe=&spy=&light=&heavy=&ram=&catapult=&knight=&snob=&x=525&y=473&target_type=coord&input=&attack=Attack
+            // Response html is in attacjResponseHtml
+            var str = RequestManager.GetResponseStringFromResponse(res);
+            CQ html = str;
+            var formElement = html.Select("#command-data-form");
+            var inputs = formElement.Children("input").Where(ele => ele.Type == "hidden").ToList();
+
+            var queryString = inputs.Aggregate("", (current, input) => current + $"{input.Name}={input.Value}&");
+            return queryString;
+        }
+
+
+        private void SendAttackConfirm(string postData, string token) {
+            var url = $"https://sv36.tribalwars.se/game.php?village=2173&screen=place&action=command&h={token}";
+            var req = _requestManager.GeneratePOSTRequest(url, postData, null, null, true);
+            var res = _requestManager.GetResponse(req);
+            var htmlResponse = RequestManager.GetResponseStringFromResponse(res);
+        }
 
     }
 
@@ -10,6 +61,7 @@
 
         public int X { get; set; }
         public int Y { get; set; }
+        public UnitsGroup units { get; set; }
 
     }
 
@@ -25,6 +77,11 @@
         public int Catapult { get; set; }
         public int Knight { get; set; }
         public int Snob { get; set; }
+
+        public override string ToString() {
+            return
+                $"spear={Spear}&sword={Sword}&axe={Axe}&spy={Spy}&light={Light}&heavy={Heavy}&ram={Ram}&catapult={Catapult}&knight={Knight}&snob={Snob}";
+        }
 
     }
 
